f0fb985c09f9647976ded7af9963d18372ddee8e
unknown <exgin3538@yahoo.com>
2020-06-03 13:50:52 -0400

    add post schema validator

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const Book = require("../models/book");
-
+const express = require('express');
+const Book = require('../models/book');
 const router = new express.Router();
-
+const jsonschema = require('jsonschema');
+const bookSchema = require('../schemas/bookSchema.json');
+const ExpressError = require('../expressError');
 
 /** GET / => {books: [book, ...]}  */
 
-router.get("/", async function (req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
     const books = await Book.findAll(req.query);
     return res.json({ books });
@@ -17,7 +18,7 @@ router.get("/", async function (req, res, next) {
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
+router.get('/:id', async function (req, res, next) {
   try {
     const book = await Book.findOne(req.params.id);
     return res.json({ book });
@@ -28,10 +29,18 @@ router.get("/:id", async function (req, res, next) {
 
 /** POST /   bookData => {book: newBook}  */
 
-router.post("/", async function (req, res, next) {
+router.post('/', async function (req, res, next) {
   try {
-    const book = await Book.create(req.body);
-    return res.status(201).json({ book });
+    const jsonval = jsonschema.validate(req.body, bookSchema);
+
+    if (!jsonval.valid) {
+      let listOfErros = jsonval.errors.map((error) => error.stack);
+      let error = new ExpressError(listOfErros, 400);
+      return next(error);
+    } else {
+      const book = await Book.create(req.body);
+      return res.status(201).json({ book });
+    }
   } catch (err) {
     return next(err);
   }
@@ -39,7 +48,7 @@ router.post("/", async function (req, res, next) {
 
 /** PUT /[isbn]   bookData => {book: updatedBook}  */
 
-router.put("/:isbn", async function (req, res, next) {
+router.put('/:isbn', async function (req, res, next) {
   try {
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
@@ -50,10 +59,10 @@ router.put("/:isbn", async function (req, res, next) {
 
 /** DELETE /[isbn]   => {message: "Book deleted"} */
 
-router.delete("/:isbn", async function (req, res, next) {
+router.delete('/:isbn', async function (req, res, next) {
   try {
     await Book.remove(req.params.isbn);
-    return res.json({ message: "Book deleted" });
+    return res.json({ message: 'Book deleted' });
   } catch (err) {
     return next(err);
   }
